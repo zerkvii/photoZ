bf9e38160e5433b2978be824f8dcad87db7ffeb9
sys <sys@whu.edu.cn>
2018-12-13 16:49:05 +0800

    sec

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = True
+db = SQLAlchemy(app)
+
+
+class Class_entity(db.Model):
+    __tablename__ = 'classes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+
+
+class Semaster(db.Model):
+    __tablename__ = 'semasters'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    teacher = db.Column(db.String(12), nullable=False)
+    students_num = db.Column(db.Integer, nullable=False)
+    time_serial_id = db.Column(db.Integer, nullable=False)
+
+    class_entity_id = db.Column(db.Integer, db.ForeignKey('classes.id'))
+    class_entity = db.relationship('Class_entity', backref=db.backref('courses'), lazy=True)
+
+
+class Time_serial(db.Model):
+    __tablename__ = 'time_serials'
+    id = db.Column(db.Integer, primary_key=True)
+    start_week = db.Column(db.Integer, nullable=False)
+    end_week = db.Column(db.Integer, nullable=False)
+    start_class = db.Column(db.Integer, nullable=False)
+    end_class = db.Column(db.Integer, nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
