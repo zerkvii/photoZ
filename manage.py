22c7d2aa6497846968bd89b023cf70e4402d64ff
ubuntu-zerk <zerk@ubuntu.com>
2018-12-14 20:34:14 +0800

    login

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
@@ -6,6 +6,7 @@ from models import db
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['FLASK_ENV'] = 'development'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost:3306/lab'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -20,8 +21,11 @@ def create_db():
     db.create_all()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # data=request.
+        print(request.get_data())
     return render_template('index.html')
 
 
--- models.py
+++ models.py
@@ -50,6 +50,7 @@ class Time_serial(db.Model):
 
 
 class User(db.Model):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
